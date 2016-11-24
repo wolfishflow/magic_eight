7577367196746c72c6dff7f1902fbd3782ff1674
Muska Ahmadzai <officialmuska@hotmail.com>
2016-11-24 13:19:56 -0500

    Created generic OpenGL environment with GL Surface activity launched as MAIN

---
+++ MyApplication/app/src/main/java/codebusters/magic_eight/MyGLRenderer.java
@@ -0,0 +1,33 @@
+package codebusters.magic_eight;
+
+import android.opengl.EGLConfig;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by ahmadzam on 2016-11-24.
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+}
---
+++ MyApplication/app/src/main/java/codebusters/magic_eight/MyGLSurfaceView.java
@@ -0,0 +1,23 @@
+package codebusters.magic_eight;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by ahmadzam on 2016-11-24.
+ */
+public class MyGLSurfaceView extends GLSurfaceView {
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer((Renderer) mRenderer);
+    }
+}
