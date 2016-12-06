da7f1bf210a73ed30d47a6fddb524d556bc2905d
alan simon <alancarlsimon@gmail.com>
2016-12-05 22:04:43 -0500

    Sam is actually Sally... o well.

--- MyApplication/app/src/main/java/codebusters/magic_eight/home/HomeFragment.java
+++ MyApplication/app/src/main/java/codebusters/magic_eight/home/HomeFragment.java
@@ -3,16 +3,19 @@ package codebusters.magic_eight.home;
 import android.app.Fragment;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import codebusters.magic_eight.R;
 import codebusters.magic_eight.dao.DatabaseConnector;
@@ -27,6 +30,9 @@ public class HomeFragment extends Fragment {
 
     DatabaseConnector db;
     private final String DEBUG_TAG = "magic_eight_debug";
+    private Button btnTTS;
+    private String fortune;
+    private TextToSpeech textToSpeech;
 
     @Nullable
     @Override
@@ -42,11 +48,10 @@ public class HomeFragment extends Fragment {
 
         BottomBar bottomBar = (BottomBar) getActivity().findViewById(R.id.bottomBar);
         db = new DatabaseConnector(getContext());
-        String sign = "", fortune;
+        String sign = "";
 
         ArrayList<User> userArrayList = db.getUsers();
-        if (userArrayList.size()>0)
-        {
+        if (userArrayList.size() > 0) {
             sign = userArrayList.get(0).getSign();
         }
         TextView txtFortune = (TextView) getActivity().findViewById(R.id.tv_horoscope);
@@ -58,6 +63,44 @@ public class HomeFragment extends Fragment {
         }
         txtFortune.setText(fortune);
 
+
+        textToSpeech = new TextToSpeech(getActivity().getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        btnTTS = (Button) getActivity().findViewById(R.id.btn_texttospeech);
+
+        btnTTS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(fortune, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
         super.onActivityCreated(savedInstanceState);
+    }
+
+
+    @Override
+    public void onPause() {
+        if(textToSpeech !=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        if(textToSpeech !=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onStop();
     }
 }
